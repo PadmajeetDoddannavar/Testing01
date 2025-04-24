c535477a49be2d945034ee90028229313ff79cf6
Padmajeet Doddannavar <155511580+PadmajeetDoddannavar@users.noreply.github.com>
2025-04-24 21:41:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
 
-// List of words for game
+// List of words for
 const words = [
   'sigh',
   'tense',
